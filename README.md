15b1f0857b21264cad73a740efc835d692e03823
Thomas Nguyen <thomas.nguyen@everyonecounts.com>
2016-04-03 23:26:27 -0700

    Website ready

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # Protractor project
 
 ### What is it?
-Protractor is an end-to-end test framework for AngularJS applications
+This is an example to Automation Web Design with Gulp
 
 
 ### Latest Version
@@ -18,25 +18,43 @@ Explication of the code: N/A
 
 ### Installation
 [Git] - optional
+
 * Download and install [Git]
 * Command prompt in a new directory: git init
 
 [nodejs]
+
 * Install [nodejs]
 * Check installation: node --version
 
 [gulp-cli]
+
 * Install [gulp-cli]: npm install -g gulp-cli
 * Check installation: gulp --version
 
+
 [npmjs]
+
 * Check installation: npm -v
 * Download the dependencies: npm install
 * Update the dependencies: npm update
 
 
 ### Running the program
-* Command prompt: gulp <task name>
+* Command prompt: gulp <task name> like "compact" (see gulpfile.js for complete list)
+
+
+### Packages recommended:
+* [tutorial 1 on Gulp.js] to optimize a website, use the following packages:
+    * gulp-util: get environment variables passed in the console to gulp 39k
+    * gulp-strip-debug: stripping console statements 0.3k
+    * gulp-autoprefixer: add vendor prefixes to CSS rules to make it compliant with older browsers 6k
+    * gulp-sass: generate CSS from SCSS 7k
+    * gulp-concat: combining all files 11k
+    * gulp-htmlmin: reducing the size of HTML 1k
+    * gulp-uglify: reducing the size of JS 12k
+    * gulp-minify-css: reducing the size of CSS files 6k
+    * gulp-imagemin: reducing the size of image 2.8k
 
 
 ### Bugs
@@ -44,8 +62,10 @@ Explication of the code: N/A
 
 
 ### References
-* [testing README.md]
-* [learning Gulp.js]
+* README.md: [README.md syntax] and [README.md live test]
+* [SASS syntax]
+* [opinion of Grunt vs Gulp] published on May 11, 2015
+
 
 
 [Git]: https://git-scm.com/downloads
@@ -55,5 +75,8 @@ Explication of the code: N/A
 [nodejs]: https://nodejs.org/en/download/
 [Webstorm]: https://www.jetbrains.com/webstorm/
 
-[learning Gulp.js]: <http://www.sitepoint.com/introduction-gulp-js/>
-[testing README.md]: <http://dillinger.io/>
+[README.md syntax]: <https://confluence.atlassian.com/bitbucketserver/markdown-syntax-guide-776639995.html>
+[README.md live test]: <http://dillinger.io/>
+[SASS syntax]: <http://sass-lang.com/guide>
+[opinion of Grunt vs Gulp]: http://sixrevisions.com/web-development/grunt-vs-gulp/
+[tutorial 1 on Gulp.js]: <http://www.sitepoint.com/introduction-gulp-js/>
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,114 @@
-// include gulp
-var gulp = require('gulp');
+// =====================================================================================================================
+// include Gulp
+// =====================================================================================================================
 
-// include plug-ins
-var jshint = require('gulp-jshint');
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    jshint  = require('gulp-jshint');
 
-// JS hint task
+// configure the jshint task
 gulp.task('jshint', function() {
     gulp.src('./src/scripts/*.js')
         .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// =====================================================================================================================
+// Web plug-ins
+// =====================================================================================================================
+
+var imagemin    = require('gulp-imagemin');
+var changed     = require('gulp-changed');
+var minifyHTML  = require('gulp-htmlmin');
+var concat      = require('gulp-concat');
+var stripDebug  = require('gulp-strip-debug');
+var uglify      = require('gulp-uglify');
+var sass        = require('gulp-sass');
+var autoprefix  = require('gulp-autoprefixer');
+var minifyCSS   = require('gulp-minify-css');
+
+
+// minify new images
+gulp.task('image', function() {
+    var imgSrc = './src/images/**/*',
+        imgDst = './build/images';
+
+    return gulp.src(imgSrc)
+        .pipe(changed(imgDst))
+        .pipe(imagemin({progressive: true}))
+        .pipe(gulp.dest(imgDst));
+});
+
+
+// minify new or changed HTML pages
+gulp.task('htmlpage', function() {
+    var htmlSrc = './src/*.html',
+        htmlDst = './build';
+
+    return gulp.src(htmlSrc)
+        .pipe(changed(htmlDst))
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(htmlDst));
+});
+
+
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+    return gulp.src(['./src/scripts/lib.js','./src/scripts/*.js'])
+        .pipe(concat('script.js'))
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts/'));
+});
+
+
+// CSS concat, auto-prefix and minify
+gulp.task('styles', function() {
+    return gulp.src(['./src/styles/*.scss'])
+        .pipe(sass())
+        .pipe(concat('styles.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./build/styles/'));
+});
+
+
+// default gulp task
+gulp.task('compact', ['image', 'htmlpage', 'scripts', 'styles'], function() {
+    // watch for images changes
+    gulp.watch('./src/images/**/*', function() {
+        gulp.run('image');
+    });
+
+    // watch for HTML changes
+    gulp.watch('./src/*.html', function() {
+        gulp.run('htmlpage');
+    });
+
+    // watch for JS changes
+    gulp.watch('./src/scripts/*.js', function() {
+        gulp.run('jshint', 'scripts');
+    });
+
+    // watch for CSS changes
+    gulp.watch('./src/styles/*.css', function() {
+        gulp.run('styles');
+    });
+});
+
+// =====================================================================================================================
+// Others
+// =====================================================================================================================
+
+// create a default task and just log a message
+gulp.task('default', function() {
+    // Run: gulp
+    // Run: gulp --env=prod
+    return gutil.env.env === 'prod'? gutil.log('Gulp is running in Prod!') : gutil.log('Gulp is running!');
+});
+
+gulp.on('stop', function () {
+    process.nextTick(function () {
+        process.exit(0);
+    });
 });
